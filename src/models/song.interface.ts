f03e58f0e096a7efdd2b2c12d0822e1ada94472d
Luis Manuel Rodriguez <lwe_@hotmail.com>
2017-10-21 17:17:05 -0500

    added interface and initial bindings

---
+++ src/models/song.interface.ts
@@ -0,0 +1,11 @@
+export interface Song {
+    title : string;
+    artist : string;
+    preview_url : string;
+    image_small : string;
+    image_medium : string;
+    image_large : string;
+    open_url : string;
+    song_id : string;
+    isFavorite?: boolean;
+}
