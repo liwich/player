64f68ee63180aa3a220432e16ff6b7d202c90ec5
Unknown <villicana.ca@gmail.com>
2017-10-25 23:05:29 -0500

    added function to play audio

--- src/pages/play-song/play-song.ts
+++ src/pages/play-song/play-song.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Song } from '../../models/song.interface';
 
+declare let Media: any;
+
 @IonicPage()
 @Component({
   selector: 'page-play-song',
@@ -13,8 +15,18 @@ export class PlaySongPage {
   public progress: number = 0;
   public isPlaying: boolean = false;
   public progressInterval: any;
+  public duration: any;
+  public completed: any;
+
+  public audio: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.song = navParams.get('song');
+    if (window['cordova'] === undefined || window['Media'] === undefined) {
+      console.log('Cordova Media is not available');
+      return;
+    };
+    this.createAudio(this.song.preview_url);
   }
 
   ionViewDidLoad() {
@@ -22,20 +34,68 @@ export class PlaySongPage {
   }
 
   ionViewDidLeave(){
-    this.pauseSong();
+    this.destroy();
   }
 
-  playSong(){
+  private createAudio(url) {
+    this.audio = new Media(url, () => {
+       console.log('Finished playback');
+       this.stopTimer();
+       this.isPlaying = false;
+       this.destroy(); 
+    }, (err) => {
+      console.log(`Audio error => track ${url}`, err);
+    }, (status) => {
+
+    });
+  }
+
+
+  public playSong() {
+    if (!this.audio) {
+      this.createAudio(this.song.preview_url);
+    }
+
     this.isPlaying = true;
+    this.audio.play();
+    this.startTimer();
+  }
+
+  private startTimer() {
     this.progressInterval = setInterval(() => {
-      this.progress < 100 ? this.progress++ : this.progress = 0;
+      if (this.duration === undefined) {
+        let duration: number = this.audio.getDuration();
+        (duration > 0) && (this.duration = Math.round(this.audio.getDuration() * 100 )/100);
+      }
+
+      this.audio.getCurrentPosition((position) => {
+            if (position > -1) {
+              this.progress = Math.round(position * 100 * 3.36)/100;
+              console.log(this.progress);
+              this.completed = this.duration > 0 ? Math.round(this.progress / this.duration * 100)/100 : 0;
+            }
+        }, (e) => {
+            console.log("Error getting position", e);
+        }
+      );
     }, 1000);
   }
 
-  pauseSong(){
-    this.isPlaying = false;
+  private stopTimer() {
     clearInterval(this.progressInterval);
   }
 
+  public pauseSong() {
+    if (!this.isPlaying) return;
+
+    this.isPlaying = false;
+    this.audio.pause();
+    this.stopTimer();
+  }
+
+  public destroy() {
+    if(!this.audio) return;
+    this.audio.release();
+  }
 
 }
